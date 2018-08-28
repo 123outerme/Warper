8612b35274d136073d67e8afa0092b8f53659132
Stephen Policelli <123outerme@outlook.com>
2018-08-28 15:54:13 -0400

    Finalized deploy script

--- z_Deploy.bat
+++ z_Deploy.bat
@@ -1,20 +1,20 @@
-@ROBOCOPY %cd% "%cd%\cs-bin\build" *.c *.h *.ico *.sh a_makeWindows.bat Makefile
-@ROBOCOPY %cd% "%cd%\cs-bin\execute" *.txt *.png *.ttf *.ogg *.mp3 /MIR /XD dirs "%cd%\cs" "%cd%\cs-bin" "%cd%\.github" "%cd%\.git" "%cd%\bin" "%cd%\builds" "%cd%\documentation" "%cd%\obj" "%cd%\media" "%cd%\docs"
-@cd cs-bin\build
+@ROBOCOPY %cd% "%cd%\warper-bin\build" *.c *.h *.ico *.sh a_makeWindows.bat Makefile
+@ROBOCOPY %cd% "%cd%\warper-bin\execute" *.txt *.png *.ttf *.ogg *.mp3 /MIR /XD dirs "%cd%\warper" "%cd%\warper-bin" "%cd%\.github" "%cd%\.git" "%cd%\bin" "%cd%\builds" "%cd%\documentation" "%cd%\obj" "%cd%\media" "%cd%\docs"
+@cd warper-bin\build
 make thenClean
-make example clean
-@copy %cd%\*.a %cd%\..\..\cs
+@copy %cd%\*.a %cd%\..\..\warper
 @move %cd%\*.a %cd%\..
-@move %cd%\*.exe %cd%\..\..\cs\execute
+@move %cd%\*.exe %cd%\..\..\warper\execute
 @cd ..\..
-@del %cd%\builds\cs-bin.zip
-@ROBOCOPY %cd% "%cd%\cs\execute" *.txt *.png *.ttf *.ogg *.mp3 /MIR /XD dirs "%cd%\cs" "%cd%\cs-bin" "%cd%\.github" "%cd%\.git" "%cd%\bin" "%cd%\builds" "%cd%\documentation" "%cd%\obj" "%cd%\media" "%cd%\docs" "%cd%\saves"
-@cd cs-bin
+@del %cd%\builds\warper-bin.zip
+@ROBOCOPY %cd% "%cd%\warper\execute" *.txt *.png *.ttf *.ogg *.mp3 /MIR /XD dirs "%cd%\warper" "%cd%\warper-bin" "%cd%\.github" "%cd%\.git" "%cd%\bin" "%cd%\builds" "%cd%\documentation" "%cd%\obj" "%cd%\media" "%cd%\docs" "%cd%\saves"
+@cd warper-bin
+@del %cd%\execute\Warper.exe
 @rem zip it up...
-@%cd%\..\7za.exe a -y -tzip "%cd%\..\builds\cs-bin.zip" "%cd%" -mx5
+@%cd%\..\7za.exe a -y -tzip "%cd%\..\builds\warper-bin.zip" "%cd%" -mx5
 @rem done...
-@del %cd%\..\builds\cs.zip
+@del %cd%\..\builds\warper.zip
 @rem zip it up...
 cd ..
-@%cd%\7za.exe a -y -tzip "%cd%\builds\cs.zip" "%cd%\cs" -mx5
+@%cd%\7za.exe a -y -tzip "%cd%\builds\warper.zip" "%cd%\warper" -mx5
 @rem done...
