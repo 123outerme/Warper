5d1124af3030cbfeee6f6452b6ede5fcfb5c6eee
Stephen Policelli <123outerme@outlook.com>
2019-01-17 14:30:29 -0500

    Added old makefile

--- z_Deploy.bat
+++ z_Deploy.bat
@@ -1,12 +1,12 @@
-@ROBOCOPY %cd% "%cd%\warper-bin\build" *.c *.h *.ico *.sh a_makeWindows.bat Makefile
+@ROBOCOPY %cd% "%cd%\warper-bin\build" *.c *.h *.ico *.sh *.a a_makeWindows.bat Makefile
 @ROBOCOPY %cd% "%cd%\warper-bin\execute" *.txt *.png *.ttf *.ogg *.mp3 /MIR /XD dirs "%cd%\warper" "%cd%\warper-bin" "%cd%\.github" "%cd%\.git" "%cd%\bin" "%cd%\builds" "%cd%\documentation" "%cd%\obj" "%cd%\media" "%cd%\docs"
 @cd warper-bin\build
 make thenClean
 @copy %cd%\*.a %cd%\..\..\warper
 @move %cd%\*.a %cd%\..
 @move %cd%\*.exe %cd%\..\..\warper\execute
-@cd ..\..
 @del %cd%\builds\warper-bin.zip
+@cd ..\..
 @ROBOCOPY %cd% "%cd%\warper\execute" *.txt *.png *.ttf *.ogg *.mp3 /MIR /XD dirs "%cd%\warper" "%cd%\warper-bin" "%cd%\.github" "%cd%\.git" "%cd%\bin" "%cd%\builds" "%cd%\documentation" "%cd%\obj" "%cd%\media" "%cd%\docs" "%cd%\saves"
 @cd warper-bin
 @del %cd%\execute\Warper.exe
